3d3f3b570c0041b583e65382422be40469c44959
josh jankowski <joshjankowski@gmail.com>
2022-08-16 20:11:23 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Text-Editor
+#### Table of Contents
+
+[Text Editor Pro](#Text Editor Pro)
+[User Story](#user-story)
+[Motivation](#motivation)
+[Uniqueness](#uniqueness)
+[Preview Photo](#preview-photo)
+[Usage](#usage)
+[Git Hub URL](#git-hub-URL)
+[Author](#author)
+[Email](#email)
+[License](#license)
+[Badge](#badge)
+## [Text Editor Pro
+
+This is a text editor that will save your information within the web browser using the browsers casche.  
+
+## User Story
+
+You will be able to save important text and edit text with this editor. You can also save it to your desktop.
+
+## Motivation
+
+This will be useful for those that need a simple way to be able to proofread text. Especially for those that do not have Word.
+
+## Uniqueness
+
+Its not.
+
+## Preview Photo
+
+![img](./photo.PNG)
+
+## Usage
+
+They can use it to save text.
+
+## Git Hub URL
+
+https://github.com/joshjankowski
+
+## Author
+
+Josh Jankowski
+
+## Email
+
+[mailto](mailto:joshjankowski@gmail.com)
+
+## License
+
+This project is protected by the ISC license. Copyright J Cubed LLC
+
+## Badge
+
+![img](https://img.shields.io/badge/Javascript-PWA-orange)
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,24 +1,37 @@
+const { application } = require("express");
+const e = require("express");
+
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-  butInstall.classList.toggle("hidden", false);
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  butInstall.style.display = 'block';
+
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
-  }
-  promptEvent.prompt();
-  window.deferredPrompt = null;
-  butInstall.classList.toggle("hidden", true);
+butInstall.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  // butInstall.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } 
+    // else {
+    //   console.log('User dismissed the A2HS prompt');
+    // }
+    deferredPrompt = null;
+  });
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+    app.logEvent('a2hs', 'installed')
 });
